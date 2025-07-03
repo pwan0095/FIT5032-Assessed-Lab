9c9753cb13e30c584cd12368ac7c42dae18872fa
pwan0095 <pwan0095@student.monash.edu>
2025-07-03 14:21:42 +0800

    Complete Assessed Lab 2 project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// 1. 导入你正在编写的 JSON.vue 组件
+import JSONComponent from './components/JSON.vue'
+</script>
+
+<template>
+  <main>
+    <JSONComponent />
+  </main>
+</template>
+
+<style scoped>
+/* 样式可以保持原样或清空，暂时不影响功能 */
+</style>
